433ef5de9b0c1f38927ad788b926751ab3d86a5f
JC <thawedbuffalo@gmail.com>
2024-05-13 09:57:12 -0400

    first commit

---
+++ src/main/java/tbs/tutorialspringrestwebsevc/TutorialSpringRestWebsevcApplication.java
@@ -0,0 +1,13 @@
+package tbs.tutorialspringrestwebsevc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringRestWebsevcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringRestWebsevcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tbs/tutorialspringrestwebsevc/TutorialSpringRestWebsevcApplicationTests.java
@@ -0,0 +1,13 @@
+package tbs.tutorialspringrestwebsevc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialSpringRestWebsevcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
